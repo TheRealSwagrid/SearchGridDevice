f3bb50ef6b145bfefadedd2b36a32755e3df915f
Luca A <luca.alfano@student.uni-augsburg.de>
2021-12-15 15:16:30 +0100

    added func

--- SearchGridDevice.py
+++ SearchGridDevice.py
@@ -1,24 +1,21 @@
 import signal
+import sys
 from threading import Thread
 from time import sleep
 
 from AbstractVirtualCapability import AbstractVirtualCapability, VirtualCapabilityServer
 
-
-class TestField(AbstractVirtualCapability):
-
+class SearchGridDevice(AbstractVirtualCapability):
     def __init__(self, server):
         super().__init__(server)
+        self.ISSECopterPosition = [0., 0., 0.]
 
-    def execute_command(self, command: dict):
-        if command["capability"] is "GetTestFieldBoundaries":
-            pass
-        elif command["capability"] is "SetTestFieldBoundaries":
-            pass
+    def SearchGridGetNextPosition(self, params: dict) -> dict:
+        formatPrint(self, f"Calculating new Position to fly to from: {params}")
+        return {"Position3D": [0., 0., 0.]}
 
     def loop(self):
-        sleep(5)
-        self.send_message({"alive":True})
+        pass
 
 
 if __name__ == "__main__":
@@ -29,12 +26,16 @@ if __name__ == "__main__":
         quit(1)
 
     try:
-        server = VirtualCapabilityServer()
-        listener = TestField(server)
+        port = None
+        if len(sys.argv[1:]) > 0:
+            port = int(sys.argv[1])
+        server = VirtualCapabilityServer(port)
+        listener = SearchGridDevice(server)
         listener.start()
         signal.signal(signal.SIGTERM, handler)
         listener.join()
         # Needed for properly closing, when program is being stopped wit a Keyboard Interrupt
     except KeyboardInterrupt:
         print("[Main] Received KeyboardInterrupt")
+        server.kill()
         listener.kill()

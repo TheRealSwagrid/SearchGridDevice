1d9d86331b2498aed0389e710751abc4c24f3fc8
Luca A <luca.alfano@student.uni-augsburg.de>
2022-02-22 02:00:41 +0100

    1: +SuperCaps can now trigger SubCaps

--- SearchGridDevice.py
+++ SearchGridDevice.py
@@ -1,9 +1,10 @@
+import json
 import signal
 import sys
 from threading import Thread
 from time import sleep
 
-from AbstractVirtualCapability import AbstractVirtualCapability, VirtualCapabilityServer
+from AbstractVirtualCapability import AbstractVirtualCapability, VirtualCapabilityServer, formatPrint
 
 class SearchGridDevice(AbstractVirtualCapability):
     def __init__(self, server):
@@ -12,10 +13,16 @@ class SearchGridDevice(AbstractVirtualCapability):
 
     def SearchGridGetNextPosition(self, params: dict) -> dict:
         formatPrint(self, f"Calculating new Position to fly to from: {params}")
+        self.invoke_async("GetISSECopterPosition", {}, self.asyncFunc)
+        formatPrint(self, json.dumps(self.invoke_sync("GetTestFieldBoundaries", {})) + " FROM SYNCED")
         return {"Position3D": [0., 0., 0.]}
 
     def loop(self):
         pass
+
+    def asyncFunc(self, params: dict):
+        formatPrint(self, f"Async Function got sth: {json.dumps(params)}")
+
 
 
 if __name__ == "__main__":

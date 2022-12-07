739178b453f6731d85b3091554742a23fcc71738
Luca Alfano(HIWI) <luca.alfano@isse.de>
2022-12-07 14:48:15 +0100

    + small bugfix

--- SearchGridDevice.py
+++ SearchGridDevice.py
@@ -13,7 +13,7 @@ class SearchGridDevice(AbstractVirtualCapability):
         self.uri = "SearchGridDevice"
         self.ISSECopterPosition = [0., 0., 0.]
         self.last_position = [0,0]
-        self.res = 4
+        self.res = 6
 
     def SearchGridGetNextPosition(self, params: dict) -> dict:
         self.ISSECopterPosition = self.invoke_sync("GetISSECopterPosition", {})["Position3D"]
@@ -55,23 +55,14 @@ class SearchGridDevice(AbstractVirtualCapability):
 
 
 if __name__ == "__main__":
-    # Needed for properly closing when process is being stopped with SIGTERM signal
-    def handler(signum, frame):
-        print("[Main] Received SIGTERM signal")
-        listener.kill()
-        quit(1)
-
     try:
         port = None
         if len(sys.argv[1:]) > 0:
             port = int(sys.argv[1])
         server = VirtualCapabilityServer(port)
-        listener = SearchGridDevice(server)
-        listener.start()
-        signal.signal(signal.SIGTERM, handler)
-        listener.join()
+        sgd = SearchGridDevice(server)
+        sgd.start()
+        sgd.join()
         # Needed for properly closing, when program is being stopped wit a Keyboard Interrupt
-    except KeyboardInterrupt:
-        print("[Main] Received KeyboardInterrupt")
-        server.kill()
-        listener.kill()
+    except Exception as e:
+        print(f"[ERROR] {repr(e)}")
